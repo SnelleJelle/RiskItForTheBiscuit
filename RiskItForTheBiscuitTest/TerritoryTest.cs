cd5d914b06d802210ca14ea04bdfe319900e466f
Jelle Criel <jelle.criel@gmail.com>
2016-02-20 21:33:19 +0100

    added first unit test

---
+++ RiskItForTheBiscuitTest/TerritoryTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RiskItForTheBiscuit.Risk;
+
+namespace RiskItForTheBiscuitTest
+{
+    [TestClass]
+    public class TerritoryTest
+    {
+        Territory territory;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            territory = new Territory("test");
+        }
+
+        [TestMethod]
+        public void TestName()
+        {
+            Assert.AreEqual("test", territory.Name);
+        }
+    }
+}
