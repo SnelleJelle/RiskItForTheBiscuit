b64f2f57ff8f67034e3f473c131f5b0197da93f9
Jelle Criel <jelle.criel@gmail.com>
2016-06-10 12:39:26 +0200

    started moving labels to seperate custom control

---
+++ RiskItForTheBiscuitClient/TerritoryLabel.cs
@@ -0,0 +1,42 @@
+﻿using RiskItForTheBiscuit.Risk;
+using RiskItForTheBiscuitClient.Drawing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RiskItForTheBiscuitClient
+{
+    public partial class TerritoryLabel : Control
+    {
+        public Territory Territory { get; set; } = new Territory("sample")
+        {
+            NrOfSoldiers = 1,
+            Owner = new Player("")
+            {
+                PlayerColor = Color.AliceBlue
+            }
+        };
+
+        public TerritoryLabel()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+
+            Graphics g = pe.Graphics;
+
+            Rectangle r = new Rectangle(0, 0, 30, 12);
+            g.DrawRectangle(SystemPens.ActiveBorder, r);
+            g.DrawLabel(Territory);
+        }
+    }
+}
